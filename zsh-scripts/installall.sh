9328ac458ccd693a6afb1ab81c126915c8b49741
Mikko <mikko@mfabrik.com>
2012-05-07 13:28:34 +0300

    Public release

---
+++ zsh-scripts/installall.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+echo "Checking your shell"
+
+(if [[ '1' == '1' ]]
+then
+    exit 0
+fi
+exit 1) > /dev/null 2>&1
+
+if test "$?" "!=" "0"
+then
+    echo "You are using some borken shell, try bash, zsh!"
+    exit 1
+fi
+
+echo -n "Checking which installation program to use: "
+
+if which apt-get
+then
+	INSTCMD="apt-get install"
+elif which yast
+then
+	INSTCMD="yast -i"
+fi
+
+echo "Installing necessary commands... "
+
+function doinstall {
+    echo -ne "  * $1... "
+    which "$1" > /dev/null 2>&1
+    if [[ "$?" == "0" ]]
+    then
+        echo "installed."
+        return
+    fi
+    echo "not installed."
+    sudo $INSTCMD $1
+}
+
+function mandatory {
+    doinstall $1
+    if [[ "$?" != "0" ]]
+    then
+        echo "Error 1: Installation of $1 failed" >&2
+        exit 1
+    fi
+}
+
+function optional {
+    doinstall $1
+}
+
+mandatory zsh
+mandatory git
+optional lesspipe
+optional wget
+optional highlight
+
+echo "Installation complete."
+echo "Checking out new shell files"
+
+mkdir ~/tools
+cd ~/tools
+git init
+git pull http://haapala.iki.fi/tools/.git
+~/tools/zsh-scripts/setup.zsh
---
+++ zsh-scripts/setup.zsh
@@ -0,0 +1,41 @@
+#!/bin/zsh
+
+BASE_DIR="$1"
+if [[ "$BASE_DIR" == "" ]]
+then
+    BASE_DIR=$HOME
+fi
+
+if [[ ! -e "$BASE_DIR/.zsh" ]]
+then
+        if [[ -e $BASE_DIR/tools/zsh-scripts ]]
+        then
+            ln -s tools/zsh-scripts $BASE_DIR/.zsh
+	else
+	    echo "$BASE_DIR/.zsh or $BASE_DIR/tools/zsh-scripts - the directory does not exist!"
+	    echo "Did you do the git pull properly?"
+            exit 1
+        fi
+fi
+
+ZSHRC="$BASE_DIR/.zshrc"
+
+if [[ -h "$ZSHRC" ]]
+then
+	rm "$ZSHRC"
+elif [[ -e "$ZSHRC" ]]
+then
+	echo "Backing up old zshrc to zshrc.backup"
+	mv "$ZSHRC" "$ZSHRC".backup
+fi
+
+echo "Copying .zshrc"
+cp "$BASE_DIR/.zsh/zshrc.template" "$BASE_DIR/.zshrc"
+
+echo "Compiling rc-files"
+zsh "$BASE_DIR"/tools/zsh-scripts/scripts/compile.zsh
+
+echo "Checking installed helper programs"
+zsh "$BASE_DIR/.zsh/bin/checkinstalledapps" "$BASE_DIR"
+
+echo "All done - enjoy!"
