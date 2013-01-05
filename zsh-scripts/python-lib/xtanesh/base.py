6ff1d95b416d88f531c1697ac8fab639e1aa9658
Antti Haapala <antti@haapala.name>
2013-01-05 02:12:36 +0200

    some rudimentary communication works :P

--- zsh-scripts/python-lib/xtanesh/base.py
+++ zsh-scripts/python-lib/xtanesh/base.py
@@ -23,14 +23,37 @@ class XClientBase(object):
         super(XClientBase, self).__init__(*a, **kw)
         self.display = Xlib.display.Display(display_name)
         self.root = self.display.screen().root
-        self.XTANESH_ATOM = self.get_xtanesh_atom(create=True)
+        self.XTANESH_WINDOW  = self.display.intern_atom('_XTANESH_WINDOW_ID', only_if_exists=False)
+        self.XTANESH_COMMAND = self.display.intern_atom('_XTANESH_COMMAND', only_if_exists=False)
+        self.UTF8_STRING = self.display.intern_atom('UTF8_STRING', only_if_exists=False)
 
     def error(self, code=1, *args):
         print(*args, file=sys.stderr)
         sys.exit(code)
 
-    def get_xtanesh_atom(self, create=False):
-        return self.display.intern_atom('_XTANESH_WINDOW_ID', only_if_exists=not create)
+    def set_UTF8_property(self, window, atom, value):
+        if not isinstance(value, str):
+            value = value.encode('UTF-8')
+
+        return window.change_property(
+            atom,
+            self.UTF8_STRING,
+            8,
+            value
+        )
+
+    def get_UTF8_property(self, window, atom, delete=False):
+        value = window.get_property(
+            atom,
+            self.UTF8_STRING,
+            0,
+            65536,
+            delete=delete
+        )
+        if not value:
+            return None
+
+        return value.value
 
     def set_int_property(self, window, atom, type, value):
         window.change_property(
@@ -66,13 +89,15 @@ class XClientBase(object):
 
     def is_server_running(self):
         try:
-            server_hwnd = self.get_int_property(self.root, self.XTANESH_ATOM, Xatom.WINDOW)
+            server_hwnd = self.get_int_property(self.root, self.XTANESH_WINDOW, Xatom.WINDOW)
             if not server_hwnd:
                 return False
 
             the_window = self.id_to_window(server_hwnd)
             if the_window.get_wm_name() != self.WM_NAME:
                 return False
+
+            self.server_window = the_window
 
             return True
                 
--- zsh-scripts/python-lib/xtanesh/server.py
+++ zsh-scripts/python-lib/xtanesh/server.py
@@ -28,17 +28,17 @@ class Server(base.XClientBase, Thread):
             X.InputOutput,
             X.CopyFromParent,
             event_mask=(
-                X.StructureNotifyMask
+                X.StructureNotifyMask |
+                X.PropertyChangeMask  
             )
         )
 
         self.window.set_wm_protocols([self.WM_DELETE_WINDOW])
         self.window.set_wm_name(self.WM_NAME)
 	self.window.set_wm_icon_name(self.WM_NAME)
-
         self.set_int_property(
             self.root,
-            self.XTANESH_ATOM,
+            self.XTANESH_WINDOW,
             Xatom.WINDOW,
             self.window.__window__()
         )
@@ -47,8 +47,26 @@ class Server(base.XClientBase, Thread):
         self.event_loop()
 
     def event_loop(self):
+        self.display.flush()
         while True:
-            event = self.display.next_event()
+            e = self.display.next_event()
+
+            # Window has been destroyed, quit
+            if e.type == X.DestroyNotify:
+                sys.exit(0)
+
+            # Somebody wants to tell us something
+            elif e.type == X.ClientMessage:
+                if e.client_type == self.WM_PROTOCOLS:
+                    fmt, data = e.data
+                    if fmt == 32 and data and data[0] == self.WM_DELETE_WINDOW:
+                        sys.exit(0)
+
+            elif e.type == X.PropertyNotify and e.atom == self.XTANESH_COMMAND:
+                command_value = self.get_UTF8_property(
+                    self.window, self.XTANESH_COMMAND
+                )
+                print command_value
 
     def cleanup(self):
         try:
@@ -59,16 +77,6 @@ class Server(base.XClientBase, Thread):
 
 def __main__(args):
     server = Server()
-    try:
-        server.arguments = args
-        server.start()
-        while True:
-            server.join(600)
-            if not server.isAlive():
-                break
-
-    except KeyboardInterrupt:
-        print >> sys.stderr, "Quitting"
-    finally:
-        server.cleanup()
-    
+    server.run()
+    server.cleanup()
+    sys.exit(0)
