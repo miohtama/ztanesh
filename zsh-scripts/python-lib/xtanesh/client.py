62fbb6244fa7050479a2ebb5bd4242e8cebb5141
Antti Haapala <antti@haapala.name>
2013-01-05 02:12:36 +0200

    some rudimentary communication works :P

--- zsh-scripts/python-lib/xtanesh/client.py
+++ zsh-scripts/python-lib/xtanesh/client.py
@@ -1,13 +1,26 @@
 from . import base
 
+import json
+
 class Client(base.XClientBase):
     def __init__(self, *a, **kw):
         super(Client, self).__init__(*a, **kw)
-        self.XTANESH_ATOM = self.get_xtanesh_atom(create=True)
+        self.server_window = None
 
     def run(self, arguments):
         if not self.is_server_running():
             self.error('ZtaneSH extensions not present')
+
+        self.send_command({'foo': 'bar'})
+
+    def send_command(self, data):
+        serialized = json.dumps(data)
+        self.set_UTF8_property(
+                self.server_window,
+                self.XTANESH_COMMAND,
+                serialized
+        )
+        self.display.flush()
 
 def __main__(args):
     client = Client()
